90f26d2e00ce5976fae03ba781bbd2674ec6b353
jandrwiega <60856871+jandrwiega@users.noreply.github.com>
2021-08-29 19:51:40 +0200

    podcast layout added

--- client/pages/components/podcast.jsx
+++ client/pages/components/podcast.jsx
@@ -1,8 +1,24 @@
+import styles from '../../styles/Podcast.module.css'
+import PlayArrowOutlinedIcon from '@material-ui/icons/PlayArrowOutlined';
+import BookmarkBorderOutlinedIcon from '@material-ui/icons/BookmarkBorderOutlined';
+
 const Podcast = () => {
     return ( 
-        <>
-            <h1>Podcast</h1>
-        </>
+        <div className={styles.podcastWrapper}>
+            <div className={styles.podcastPhoto}>
+                <img src="https://placeimg.com/150/150/any" alt="" className={styles.podcastImage}/>
+            </div>
+
+            <div className={styles.podcastContent}>
+                <p className={styles.podcastAuthor}>sdaosakoaskdok kasdokoaskdasokok </p>
+                <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h2>
+            </div>
+
+            <div className={styles.podcastSettings}>
+                <div className={styles.podcastBtn}><PlayArrowOutlinedIcon className={styles.podcastBtnIcon}/></div>
+                <div className={styles.podcastBtn}><BookmarkBorderOutlinedIcon className={styles.podcastBtnIcon}/></div>
+            </div>
+        </div>
      );
 }
  
